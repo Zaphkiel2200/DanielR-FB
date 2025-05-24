07dfe9f86d9460c0380bb2069544e9f6f761d689
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2025-05-23 22:03:34 -0500

    add pages

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import Root from "./Root/Root";
+import Header from "./components/Header/Header";
+import DashboardPage from "./pages/DashboardPage";
+
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+customElements.define('root-element', Root);
+customElements.define('header-element', Header);
+customElements.define('landing-page', LandingPage);
+customElements.define('login-page', LoginPage);
+customElements.define('register-page', RegisterPage);
+customElements.define('dashboard-page', DashboardPage);
